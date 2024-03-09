220a66107539ec2eae81de4c2e199df433316b24
MartenFrehse <martenfrehse11@gmail.com>
2024-03-09 23:08:11 +0100

    add an upload app
    add upload to dev and prod

--- website/django_website/settings.py
+++ website/django_website/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'upload',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +126,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# --
+CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
