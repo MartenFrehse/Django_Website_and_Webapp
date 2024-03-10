d261d529b258de16a8587611f9503d0b67c32005
MartenFrehse <martenfrehse11@gmail.com>
2024-03-10 21:23:15 +0100

    Update settings.py
    add the templates folder of the new app

--- website/django_website/settings.py
+++ website/django_website/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'django_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'main', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
