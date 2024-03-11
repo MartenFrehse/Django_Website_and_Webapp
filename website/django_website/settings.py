307e3617206764a75aad38219eaabf6e59ef8a1c
MartenFrehse <martenfrehse11@gmail.com>
2024-03-11 22:46:29 +0100

    start github actions
    - add .github/workflows
    - start configurationg github actions yml file

--- website/django_website/settings.py
+++ website/django_website/settings.py
@@ -136,4 +136,4 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # --
-CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
+# CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
