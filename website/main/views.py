58f51ebfd2fcebf9d83c4f14079e0293db8ac01a
MartenFrehse <martenfrehse11@gmail.com>
2024-03-15 12:05:33 +0100

    add API request
    link is not working yet

--- website/main/views.py
+++ website/main/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+CHANNEL_ID_HORNER_MARSCH = os.getenv('CHANNEL_ID_HORNER_MARSCH')
+
 
 def index(request):
-    return render(request, 'main/index.html', {'active_link': 'index'})
+    url = f'https://api.thingspeak.com/channels/{CHANNEL_ID_HORNER_MARSCH}/feeds.json?api_key={API_KEY}'
+    response=requests.get(url).json()
+    print(response)
+
+    channel_info = response['channel']
+    feeds = response['feeds']
+
+    return render(request, 'main/index.html', {'active_link': 'index', 'channel_info': channel_info, 'feeds': feeds})
 
 def biene(request):
     return render(request, 'main/biene.html', {'active_link': 'biene'})
