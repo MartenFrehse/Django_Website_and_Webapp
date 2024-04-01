6e1dded60a52e0222ca92ab3191d3274711b7618
MartenFrehse <martenfrehse11@gmail.com>
2024-04-01 20:46:37 +0200

    add models
    add models for the db
    add the models to the admin page
    create superuser
    --> problems with the "honig" column

--- website/main/admin.py
+++ website/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import LeerGewicht, API_Data
 
-# Register your models here.
+class API_DataAdmin(admin.ModelAdmin):
+    readonly_fields = ['id','honig']
+
+class LeerGewichtAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+
+admin.site.register(LeerGewicht, LeerGewichtAdmin)
+admin.site.register(API_Data, API_DataAdmin)
--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class LeerGewicht(models.Model):
+    id = models.AutoField(primary_key=True)
+    leer_gewicht = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.leer_gewicht}"
+    
+    class Meta:
+        verbose_name = "Leer Gewicht"
+        verbose_name_plural = "Leer Gewichte"
+
+class API_Data(models.Model):
+    id = models.AutoField(primary_key=True)
+    datum = models.DateField()
+    zeit = models.TimeField()
+    temperatur = models.DecimalField(max_digits=5, decimal_places=2)
+    gewicht = models.DecimalField(max_digits=5, decimal_places=2)
+
+    @property
+    def honig(self):
+        leer_gewicht = LeerGewicht.objects.get(id=1).leer_gewicht
+        return self.gewicht - leer_gewicht
+
+    notiz = models.TextField()
+
+    def __str__(self):
+        return f"{self.datum} - {self.zeit}"
+    
+    class Meta:
+        verbose_name = "API Daten"
+        verbose_name_plural = "API Daten"
